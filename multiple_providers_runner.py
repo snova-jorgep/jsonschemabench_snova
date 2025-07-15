24142262743cff3c94dd2340962014f66d001513
snova-luiss <luis.salazar@sambanovasystems.com>
2025-07-15 17:07:51 -0500

    update file to run multiple configs

--- multiple_providers_runner.py
+++ multiple_providers_runner.py
@@ -1,15 +1,16 @@
 import os
+import pathlib
+from datetime import datetime
+from dotenv import load_dotenv
+
 from core.bench import bench
 from core.dataset import DATASET_NAMES
 from core.utils import load_config
 from engines.openai_compatible import OpenAICompatibleEngine, OpenAICompatibleConfig
-from dotenv import load_dotenv
-from datetime import datetime
-import pathlib
 
 current_path = pathlib.Path(__file__).resolve().parent
+config_root = current_path / "configs"
 load_dotenv(current_path / ".env")
-
 current_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 def run_bench(tasks, limit, config):
@@ -27,11 +28,20 @@ def run_bench(tasks, limit, config):
     
 if __name__ == "__main__":
     start_time = datetime.now()
-    run_bench(
-        tasks = DATASET_NAMES,
-        limit = 5,
-        config = current_path / "configs" / "sambanova-llama-4-maverick.json"
-    )
+    limit = 5
+    for provider_dir in config_root.iterdir():
+        if provider_dir.is_dir():
+            for config_file in provider_dir.glob("*.json"):
+                print(f"Running benchmark for: {config_file}")
+                try:
+                    run_bench(
+                        tasks=DATASET_NAMES,
+                        limit=limit,
+                        config=config_file
+                    )
+                except Exception as e:
+                    print(f"Failed on {config_file}: {e}")
+
     end_time = datetime.now()
     delta_time = end_time - start_time
-    print(f"Ran in: {delta_time}")
+    print(f"Total time taken: {delta_time}")
